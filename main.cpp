90712f7f17db45ad4415a1e23e494454e6a4bdb0
Varad Gotkhindikar <varad7611@gmail.com>
2024-09-27 20:49:07 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,184 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char i;
+cout<<"            ";  cout<<" WELCOME TO QUIZ GAME "<<endl;
+
+
+
+
+int k;
+
+cout<<"Please enter value 1 to start game"<<endl;
+
+cin >> k;
+
+if(k==1){
+    cout<<"lets start Game"<<endl;
+}
+else {
+
+cout<<"Thank You "<<endl;
+}
+
+
+
+
+
+cout<<"           "<<endl;
+cout<<"Q1] what is square root of 9216"<<endl;
+cout<<"    \n"<<endl;
+cout<<"a) 94    "<<endl;
+cout<<"b) 96    "<<endl;
+cout<<"c) 82    "<<endl;
+cout<<"d) 98   "<<endl;
+
+
+
+cout<<"enter your answer : ";
+cin>>i ;
+
+if(i=='a'){
+cout<<" Your answer is wrong"<<endl;
+
+}
+
+else if(i=='b'){
+    cout<<" Your answer is correct "<<endl;
+}
+
+else if(i=='c'){
+    cout<<" Your answre is wrong   "<<endl;
+}
+
+else if(i=='d'){
+    cout<<"  Your answer is wrong   "<<endl;
+}
+else{
+    cout<<" invalid answer   "<<endl;
+    
+
+}
+cout<<"        "<<endl;
+char h;
+
+cout<<"Q2] If value of X = 60 degree find the value of sinX" <<endl;
+cout<<"    \n"<<endl;
+cout<<"a) 0.5    "<<endl;
+cout<<"b) 1   "<<endl;
+cout<<"c) 0    "<<endl;
+cout<<"d) 0.866   "<<endl;
+
+
+
+cout<<"enter your answer : ";
+cin>>h;
+
+if(h=='a'){
+cout<<" Your answer is wrong"<<endl;
+
+}
+
+else if(h=='b'){
+    cout<<" Your answer is wrong "<<endl;
+}
+
+else if(h=='c'){
+    cout<<" Your answre is wrong   "<<endl;
+}
+
+else if(h=='d'){
+    cout<<"  Your answer is correct   "<<endl;
+}
+else{
+    cout<<" invalid answer   "<<endl;    
+}
+cout<<""<<endl;
+
+char ch;
+
+cout<<"Q3] What is capital of India" <<endl;
+cout<<"    \n"<<endl;
+cout<<"a) Mumbai    "<<endl;
+cout<<"b) Jaipur   "<<endl;
+cout<<"c) delhi   "<<endl;
+cout<<"d) kerala  "<<endl;
+
+cout<<"enter your answer : ";
+cin>>ch;
+
+if(ch=='a'){
+cout<<" Your answer is wrong"<<endl;
+
+}
+
+else if(ch=='b'){
+    cout<<" Your answer is wrong "<<endl;
+}
+
+else if(ch=='c'){
+    cout<<" Your answre is correct  "<<endl;
+}
+
+else if(ch=='d'){
+    cout<<"  Your answer is wrong   "<<endl;
+}
+else{
+    cout<<" invalid answer   "<<endl;    
+}
+    
+    cout<<""<<endl;
+
+
+char g;
+
+cout<<"Q4] what is symbol of potassium " <<endl;
+cout<<"    \n"<<endl;
+cout<<"a) K    "<<endl;
+cout<<"b) Ps   "<<endl;
+cout<<"c) ca   "<<endl;
+cout<<"d) he  "<<endl;
+
+cout<<"enter your answer : ";
+cin>>g;
+
+if(g=='a'){
+cout<<" Your answer is Correct"<<endl;
+
+}
+
+else if(g=='b'){
+    cout<<" Your answer is wrong "<<endl;
+}
+
+else if(g=='c'){
+    cout<<" Your answre is wrong  "<<endl;
+}
+
+else if(g=='d'){
+    cout<<"  Your answer is wrong   "<<endl;
+}
+else{
+    cout<<" invalid answer   "<<endl;    
+}
+
+cout<<"       "<<endl;
+
+cout<<"          ";     cout<<"THANKS FOR PLAYING QUIZ GAME"<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
